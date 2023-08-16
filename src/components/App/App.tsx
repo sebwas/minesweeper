1af6f6d95f9ccda19185e8528f9c36e361349a83
Sebastian Wasser <sebastian.wasser@gmail.com>
2023-08-16 17:07:36 +0200

    Add fork me ribbon

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,13 +7,32 @@ import styles from './App.module.css'
 
 function App() {
 	return (
-		<GameProvider>
-			<div className={styles.app}>
-				<GameHeader />
-				<GameGrid />
-				<GameFooter />
-			</div>
-		</GameProvider>
+		<>
+			<GameProvider>
+				<div className={styles.app}>
+					<GameHeader />
+					<GameGrid />
+					<GameFooter />
+				</div>
+			</GameProvider>
+
+			<a
+				className={styles.ribbon}
+				href="https://github.com/sebwas/minesweeper"
+				target="_blank"
+			>
+				<img
+					decoding="async"
+					width="149"
+					height="149"
+					src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+					className="attachment-full size-full"
+					alt="Fork me on GitHub"
+					loading="lazy"
+					data-recalc-dims="1"
+				/>
+			</a>
+		</>
 	)
 }
 
