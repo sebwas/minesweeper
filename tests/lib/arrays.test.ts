fc062e9812694d8394d753437fc9ce329e1a8219
Sebastian Wasser <sebastian.wasser@gmail.com>
2023-08-22 12:31:43 +0200

    Add happy path test for array lib file

--- tests/lib/arrays.test.ts
+++ tests/lib/arrays.test.ts
@@ -1,6 +1,12 @@
 import { test, expect } from 'vitest'
 import { reduceToSum } from '../../src/lib/arrays'
 
+test('reduceToSum gets the sum of the array elements', () => {
+	const testArray = [1, 2, 3, 4]
+
+	expect(reduceToSum(testArray)).toBe(10)
+})
+
 test('reduceToSum ignores non-numeric values', () => {
 	const testArray = [1, 2, 3, 'a', { b: 'c' }, [6, 5], 4]
 
