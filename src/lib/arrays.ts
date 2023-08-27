21fc05264b772ee4c727382329f51827435cb7c3
Sebastian Wasser <sebastian.wasser@toptal.com>
2023-08-27 18:52:18 +0200

    Add array chunking helper

--- src/lib/arrays.ts
+++ src/lib/arrays.ts
@@ -1,3 +1,19 @@
 export function reduceToSum(a: number[]) {
 	return a.reduce((a, b) => typeof b === 'number' ? a + b : a, 0)
 }
+
+export function chunkArray<T>(a: T[], chunkSize: number) {
+	if (chunkSize <= 0) {
+		throw new Error('Chunk size invalid: must be larger than 0.')
+	}
+
+	const chunked = []
+
+	a = [...a]
+
+	while (a.length) {
+		chunked.push([...a.splice(0, chunkSize)])
+	}
+
+	return chunked
+}
--- tests/lib/arrays.test.ts
+++ tests/lib/arrays.test.ts
@@ -1,15 +1,48 @@
-import { test, expect } from 'vitest'
-import { reduceToSum } from '../../src/lib/arrays'
+import { describe, it, expect } from 'vitest'
+import { chunkArray, reduceToSum } from '../../src/lib/arrays'
 
-test('reduceToSum gets the sum of the array elements', () => {
-	const testArray = [1, 2, 3, 4]
+describe('The reduceToSum function', () => {
+	it('gets the sum of the array elements', () => {
+		const testArray = [1, 2, 3, 4]
 
-	expect(reduceToSum(testArray)).toBe(10)
+		expect(reduceToSum(testArray)).toBe(10)
+	})
+
+	it('ignores non-numeric values', () => {
+		const testArray = [1, 2, 3, 'a', { b: 'c' }, [6, 5], 4]
+
+		// @ts-expect-error We are trying to test using invalid values here.
+		expect(reduceToSum(testArray)).toBe(10)
+	})
 })
 
-test('reduceToSum ignores non-numeric values', () => {
-	const testArray = [1, 2, 3, 'a', { b: 'c' }, [6, 5], 4]
+describe('The chunkArray function', () => {
+	it('chunks an array into the defined chunks', () => {
+		const testArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-	// @ts-expect-error We are trying to test using invalid values here.
-	expect(reduceToSum(testArray)).toBe(10)
+		const chunked1 = chunkArray(testArray, 1)
+		const chunked2 = chunkArray(testArray, 2)
+		const chunked9 = chunkArray(testArray, 9)
+		const chunked20 = chunkArray(testArray, 20)
+
+		expect(chunked1).toStrictEqual([[0], [0], [0], [0], [0], [0], [0], [0], [0]])
+		expect(chunked2).toStrictEqual([[0, 0], [0, 0], [0, 0], [0, 0], [0]])
+		expect(chunked9).toStrictEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+		expect(chunked20).toStrictEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+	})
+
+	it('does not alter the original array', () => {
+		const testArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+		const chunked = chunkArray(testArray, 2)
+
+		expect(chunked).not.toBe(testArray)
+	})
+
+	it('throws an error for a bad chunk size', () => {
+		const testArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+		expect(() => chunkArray(testArray, 0)).toThrow('Chunk size invalid: must be larger than 0.')
+		expect(() => chunkArray(testArray, -1)).toThrow('Chunk size invalid: must be larger than 0.')
+	})
 })
