998273d815aa598d4186b908a6a2a01d05e4bdec
Sebastian Wasser <sebastian.wasser@toptal.com>
2023-08-29 14:30:23 +0200

    Show github link only in non-PWA mode

---
+++ src/hooks/use-only-in-pwa.ts
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const mediaMatcher = window.matchMedia('(display-mode: standalone) or (display-mode: fullscreen)')
+
+export default function useOnlyInPwa() {
+	const [isInPwa, setIsInPwa] = React.useState(mediaMatcher.matches)
+
+	React.useEffect(() => {
+		function update() {
+			setIsInPwa(mediaMatcher.matches)
+		}
+
+		mediaMatcher.addEventListener('change', update)
+
+		return () => mediaMatcher.removeEventListener('change', update)
+	}, [])
+
+	return isInPwa
+}
